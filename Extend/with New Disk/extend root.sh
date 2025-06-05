c00e21d13f4fceb0ae3f395ea853e50dad7b43f3
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-06-05 11:57:57 -0500

    Update extend root.sh

--- Extend/with New Disk/extend root.sh
+++ Extend/with New Disk/extend root.sh
@@ -5,12 +5,15 @@ disk=vdb
 vg=ubuntu-vg
 # sudo lvs
 lv=ubuntu-lv
+# This has different '-' hyphens than above!
+# Todo: need a get() Function
+lvmPath=/dev/mapper/ubuntu--vg-ubuntu--lv
 
 pvcreate /dev/$disk
 
 vgextend $vg /dev/$disk
 
-lvextend -l +100%FREE /dev/$vg/$lv
+lvextend -l +100%FREE $lvmPath
 
 # Btrfs can resize Root in-place and live!
 btrfs filesystem resize max /
